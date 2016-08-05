e7f202de60c289c2989b77464bf475f98112a454
Freatnor <jonathan.randall.taylor@gmail.com>
2016-08-04 20:34:21 -0400

    initial commit

---
+++ app/src/main/java/com/example/freatnor/oauth_lab/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.freatnor.oauth_lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
