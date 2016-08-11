8ee7fd70b72a861aa01866bfab2f4a7ff561de54
Freatnor <jonathan.randall.taylor@gmail.com>
2016-08-10 20:39:16 -0400

    working on bonus oauth (since it's not already posted...)

--- app/src/main/java/com/example/freatnor/oauth_lab/LoginActivity.java
+++ app/src/main/java/com/example/freatnor/oauth_lab/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.freatnor.oauth_lab;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,10 +24,15 @@ import okhttp3.Response;
 public class LoginActivity extends AppCompatActivity {
     WebView mWebView;
 
+    private SharedPreferences mPreferences;
+
     private final String YOUR_AUTHORIZATION_URL = "https://api.twitter.com/oauth/request_token"; //ENTER YOUR AUTHORIZATION URL HERE
 
     private final String USER_LOGIN_URL = "https://api.twitter.com/oauth/authenticate?oauth_token=";
+    private final String CALLBACK_STRING = "oauth_callback_confirmed=";
     private final String VERIFIER_STRING = "oauth_verifier=";
+    private final String OAUTH_STRING = "oauth_token=";
+    private final String OAUTH_SECRET_STRING = "oauth_token_secret=";
 
     private static final String TAG = "LoginActivity";
 
@@ -38,51 +44,83 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mWebView = (WebView) findViewById(R.id.webview);
-        
-        getOAuthTokens():
 
-        mWebView.setWebViewClient(new WebViewClient() {
+        mPreferences = getSharedPreferences(getString(R.string.shared_preferences), MODE_PRIVATE);
 
-            /*
-                YOU CAN READ THE DOCUMENTATION ON THIS METHOD TO FIND OUT WHAT IT DOES
+        if(!mPreferences.contains(TwitterAppData.TRUE_OAUTH_TOKEN) || !mPreferences.contains(TwitterAppData.TRUE_OAUTH_TOKEN_SECRET)) {
+            getOAuthTokens();
+        }
 
-                IN SHORT, IT WAITS FOR A URL REQUEST TO HAPPEN AND THEN TRIES TO INTERCEPT IT TO DO
-                SOMETHING ELSE OTHER THAN LOADING A WEB PAGE.
 
-                WE NEED THIS SO WE CAN HANDLE THE REDIRECT URL WHEN IT COMES THROUGH.
-            */
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if(url.contains(VERIFIER_STRING)){ //CHECKING TO SEE IF THE URL WE HAVE IS THE ONE WE WANT
-                    Log.i(TAG, "shouldOverrideUrlLoading: " + url);
-
-                    int index = url.indexOf(VERIFIER_STRING);
-                    Log.i(TAG, url.substring(index+1+VERIFIER_STRING.length()));
-
-                    //STRIPPING AWAY THE URL AND ONLY KEEPING THE CODE
-                    String code = url.substring(index+1+VERIFIER_STRING.length());
-                    getAccessToken(code);
-                    return true;
-                }
-                else {
-                    return false;
-                }
-            }
-
-        });
-        mWebView.loadUrl(USER_LOGIN_URL + TwitterAppData.OAUTH_TOKEN); //WHAT DO YOU THINK THE URL SHOULD BE?
 
     }
 
+    //change for the initial oauth request
     private void getOAuthTokens() {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(TwitterAppData.REQUEST_TOKEN_URL)
+                .addHeader("authorization", TwitterAppData.buildAuthorizationHeader())
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .post(null)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: Failed Token request", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(!response.isSuccessful()){
+                    throw new IOException("Bad or empty response. Unexpected Code " + response);
+                }
+                String body = response.body().string();
+                Log.i(TAG, "onResponse: " + body);
+                String token = "";
+                String secret = "";
+                if(body.contains(OAUTH_STRING)&&body.contains(OAUTH_SECRET_STRING)){
+                    //get the new token and secret
+                    int tokenIndex = body.indexOf(OAUTH_STRING);
+                    int secretIndex = body.indexOf(OAUTH_SECRET_STRING);
+                    TwitterAppData.OAUTH_TOKEN = body.substring(tokenIndex+OAUTH_STRING.length(), secretIndex - 1);
+                    TwitterAppData.OAUTH_TOKEN_SECRET = body.substring(secretIndex + OAUTH_SECRET_STRING.length(), CALLBACK_STRING.length() - 1);
+
+                    mWebView.setWebViewClient(new WebViewClient() {
+                        @Override
+                        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                            if(url.contains(VERIFIER_STRING)){ //CHECKING TO SEE IF THE URL WE HAVE IS THE ONE WE WANT
+                                Log.i(TAG, "shouldOverrideUrlLoading: " + url);
+
+                                int index = url.indexOf(VERIFIER_STRING);
+                                Log.i(TAG, url.substring(index+1+VERIFIER_STRING.length()));
+
+                                //STRIPPING AWAY THE URL AND ONLY KEEPING THE CODE
+                                String code = url.substring(index+VERIFIER_STRING.length());
+                                getAccessToken(code);
+                                return true;
+                            }
+                            else {
+                                return false;
+                            }
+                        }
+
+                    });
+                    mWebView.loadUrl(USER_LOGIN_URL + TwitterAppData.OAUTH_TOKEN); //WHAT DO YOU THINK THE URL SHOULD BE?
+
+                }
+                else{
+                    Log.e(TAG, "onResponse: bad response, no token or secret");
+                }
+            }
+        });
+
     }
 
     /**
-     * WE WILL WORK ON THIS TOGETHER
      * @param verifier THE OAUTH_VERIFIER WE RETRIEVED FROM THE REDIRECT URL
      */
     private void getAccessToken(String verifier){
-        //WE'LL WORK ON THIS TOGETHER
         OkHttpClient client = new OkHttpClient();
         RequestBody formBody = new FormBody.Builder()
                 .add("oauth_verifier", verifier)
@@ -108,15 +146,20 @@ public class LoginActivity extends AppCompatActivity {
                 String body = response.body().string();
                 Log.i(TAG, "onResponse: " + body);
                 String token = "";
-                try {
-                    JSONObject obj = new JSONObject(body);
-                    token = obj.getString("access_token");
-                    Log.i(TAG, "onResponse: parsed token - "+token);
-                } catch (JSONException e){
-                    Log.e(TAG, "onResponse: Bad JSON " + response + " " + body, e);
+                String secret = "";
+                if(body.contains(OAUTH_STRING)&&body.contains(OAUTH_SECRET_STRING)){
+                    //get the new token and secret
+                    int tokenIndex = body.indexOf(OAUTH_STRING);
+                    int secretIndex = body.indexOf(OAUTH_SECRET_STRING);
+                    TwitterAppData.TRUE_OAUTH_TOKEN = body.substring(tokenIndex+OAUTH_STRING.length(), secretIndex - 1);
+                    TwitterAppData.TRUE_OAUTH_TOKEN_SECRET = body.substring(secretIndex + OAUTH_SECRET_STRING.length());
+
+                }
+                else{
+                    Log.e(TAG, "onResponse: bad response, no token or secret");
                 }
 
-                startActivity(new Intent(LoginActivity.this, MainActivity.class).putExtra("Authorization", token));
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
 
--- app/src/main/java/com/example/freatnor/oauth_lab/TwitterAppData.java
+++ app/src/main/java/com/example/freatnor/oauth_lab/TwitterAppData.java
@@ -1,25 +1,162 @@
 package com.example.freatnor.oauth_lab;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Base64;
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  * Created by Jonathan Taylor on 8/4/16.
  */
 public class TwitterAppData {
 
-    public static final String CLIENT_ID = "";
-    public static final String CLIENT_SECRET = "";
-    public static final String CALLBACK_URL_ENCODED = "http%3A%2F%2Fcallback";
+    private static final String TAG = "TwitterAppData";
+
+    public static final String CLIENT_ID = "PVdY7UijGBN389sDyAFVLqaHy";
+    public static final String CLIENT_SECRET = "WzgC7asTfwCUIGW4yQSF1gauQwB56eOD1ooLRZXKg8AYQpqjAe";
+    public static final String CALLBACK_URL = "https://callback";
+
+    public static final String REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/access_token";
 
     public static final String AUTHORIZATION_HEADER_PREFIX = "OAuth ";
 
-    public static String OAUTH_TOKEN = "";
-    public static String OAUTH_TOKEN_SECRET = "";
+    public static final String OAUTH_VERSION = "1.0";
+    public static final String OAUTH_SIGNATURE_METHOD = "HMAC-SHA1";
+
+    public static final String TRUE_OAUTH_TOKEN = "oauth_token";
+    public static final String TRUE_OAUTH_TOKEN_SECRET = "oauth_token_secret";
+
+    private static SharedPreferences mPreferences;
 
 
-    public static String buildAuthorizationHeader(){
+
+    //method for building the authorization header, needs a sorted map of all the normal parameters and body
+    //for the signature creation
+    public static String buildAuthorizationHeader(boolean isInitialRequest, TreeMap<String, String> initialParameters,
+                                                  String method, String url, Context context){
+
+        mPreferences = context.getSharedPreferences(context.getString(R.string.shared_preferences),
+                Context.MODE_PRIVATE);
+
+        String finalHeader = AUTHORIZATION_HEADER_PREFIX;
+
+        //setup a sorted map to hold our parameters for the Authorization Header
+        TreeMap<String, String> parameters = initialParameters;
+        parameters.put("method", method);
+        parameters.put("url", url);
+
+        //parameters for the signature...
+        parameters.put("oauth_consumer_key", CLIENT_ID);
+        parameters.put("oauth_signature_method", OAUTH_SIGNATURE_METHOD);
+        parameters.put("oauth_version", OAUTH_VERSION);
+
+        //create the timestamp and later use it to create the nonce
+        long timestamp = System.currentTimeMillis();
+        parameters.put("oauth_timestamp", "" + timestamp);
+
+        //if it's not the initial request put the oauth token, otherwise do the
+        if(isInitialRequest){
+            parameters.put("oauth_callback", CALLBACK_URL);
+        } else {
+            parameters.put("oauth_token", mPreferences.getString(TRUE_OAUTH_TOKEN, null));
+        }
+
+        //start of try block for all the encodings....
+        try{
+            //loop through the map as an iterator and encode all the current strings
+            Iterator<String> iter = parameters.keySet().iterator();
+            while(iter.hasNext()){
+                String key = iter.next();
+                parameters.put(key, URLEncoder.encode(parameters.get(key), "UTF-8"));
+            }
+
+            //create the encoded nonce
+            parameters.put("oauth_nonce", createNonce(timestamp));
+
+            //create the encoded signing key
+            parameters.put("signing_key", createSigningKey());
+
+            //create the signature
+             createSignature(parameters);
+
+
+        } catch(UnsupportedEncodingException e){
+            Log.e(TAG, "buildAuthorizationHeader: something happened while encoding the parameters", e);
+        }
+
+        Iterator<String> iter = parameters.keySet().iterator();
+        while(iter.hasNext()){
+            String key = iter.next();
+            //if the key is for oauth
+            if(key.contains("oauth")){
+                finalHeader += (key + "=" + parameters.get(key) + ", ");
+            }
+        }
+
 
     }
 
-    private static String createNonce(){
+    private static void createSignature(TreeMap<String, String> parameters) throws UnsupportedEncodingException {
+        String baseSignature = parameters.get("method") + "&" + parameters.get("url") + "&";
+        String signature;
 
+        //loop through and concatenate
+        Iterator<String> iter = parameters.keySet().iterator();
+        while(iter.hasNext()){
+            String key = iter.next();
+            if(!key.equals("method") && !key.equals("url") && !key.equals("signing_key")){
+                baseSignature += (key + "=" + parameters.get(key) + "&";
+            }
+        }
+        baseSignature = baseSignature.substring(0, baseSignature.length() -2);
+        Log.d(TAG, "createSignature: created base signature - " + baseSignature);
+
+        //HmacUtils.hmacSha1Hex(key, string_to_sign);
+        try {
+            SecretKeySpec secretKey = new SecretKeySpec(parameters.get("signing_key").getBytes("UTF-8"), "HmacSHA1");
+            Log.d(TAG, "createSignature: secret key - " + secretKey.toString());
+            Mac mac = Mac.getInstance("HmacSHA1");
+            mac.init(secretKey);
+            byte[] hmacData = mac.doFinal(baseSignature.getBytes("UTF-8"));
+            parameters.put("oauth_signature", Base64.encodeToString(hmacData, Base64.NO_WRAP));
+        } catch (NoSuchAlgorithmException e){
+            Log.e(TAG, "createSignature: bad algorithm", e);
+        } catch (InvalidKeyException e){
+            Log.e(TAG, "createSignature: invalid key", e);
+        }
+
+        parameters.put("oauth_signature", signature);
     }
+
+    private static String createNonce(long time){
+        String nonce = Base64.encodeToString(ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(time).array(),Base64.NO_WRAP);
+        Log.d(TAG, "createNonce: " + nonce);
+        return nonce;
+    }
+
+    private static String createSigningKey() throws UnsupportedEncodingException{
+        String result = "";
+        result += URLEncoder.encode(CLIENT_SECRET, "UTF-8") + "&";
+        if(mPreferences.contains(TRUE_OAUTH_TOKEN_SECRET)){
+            result += URLEncoder.encode(mPreferences.getString(TRUE_OAUTH_TOKEN_SECRET, null), "UTF-8");
+        }
+        Log.d(TAG, "createSigningKey: " + result);
+        return result;
+    }
+
+
 }
